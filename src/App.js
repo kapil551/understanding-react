290145cd9b0758b5dc76c92c752bf5390e318c6d
Kapil Chaudhary <chaudharikapil16@gmail.com>
2022-02-07 23:31:03 +0530

    Event Handling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import ClassClick from './components/ClassComponents/ClassClick';
 import SetState from './components/ClassComponents/SetState';
 import State from './components/ClassComponents/State';
+import FunctionClick from './components/FunctionalComponents/FunctionClick';
 import Greet from './components/Greet';
 import User from './components/User';
 import Welcome from './components/Welcome';
@@ -24,7 +26,7 @@ function App() {
       {/* <WithoutJSX /> */}
 
       {/* Passing Props to a component */}
-      <User name="Clark" heroName="Superman" >
+      {/* <User name="Clark" heroName="Superman" >
         <p> 
           Superpower: flight, superhuman strength, x-ray vision, heat vision, cold breath, super-speed, enhanced hearing, and nigh-invulnerability.
         </p> 
@@ -33,13 +35,17 @@ function App() {
         <button> Call Batmobile </button> 
       </User>
       <User />
-      <User name="Diana" heroName="Wonder Woman" />
+      <User name="Diana" heroName="Wonder Woman" /> */}
 
       {/* State */}
       {/* <State /> */}
 
       {/* setState */}
       {/* <SetState /> */}
+
+      {/* event handling */}
+      <FunctionClick />
+      <ClassClick />
 
 
     </div>
---
+++ src/components/ClassComponents/ClassClick.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class ClassClick extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         
+      }
+    }
+
+    clickHandler = () => {
+        console.log('Clicked the button');
+    }
+    
+    render() {
+        return <div>
+            <button onClick={() => this.clickHandler()}>Click me</button>
+        </div>;
+    }
+}
+
+export default ClassClick;
